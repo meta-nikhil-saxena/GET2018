a38579d3dc4066ca7a9141b8ba0b93595328f1f4
meta-nikhil-saxena <41181690+meta-nikhil-saxena@users.noreply.github.com>
2018-08-10 09:20:31 +0530

    Update TestDictionary.java

--- DS-Session5-Algorithm1-Assignment/src/algorithm/TestDictionary.java
+++ DS-Session5-Algorithm1-Assignment/src/algorithm/TestDictionary.java
@@ -1,12 +1,13 @@
 package algorithm;
 
 import static org.junit.Assert.*;
-
 import java.io.FileNotFoundException;
+import java.util.List;
 
 import org.junit.Before;
 import org.junit.Test;
 
+//Test class for testing Dictionary
 public class TestDictionary {
 	Dictionary dict;
 
@@ -16,12 +17,60 @@ public class TestDictionary {
 	}
 
 	@Test
-	public void testForaddWord() {
+	public void testForAddWord1() {
 		assertEquals(true, dict.addWord("Nik", "aaaaa"));
 		assertEquals(true, dict.addWord("A", "AAAAaaaaa"));
 		assertEquals(true, dict.addWord("Nikkkk", "aaaaa"));
-		assertEquals(true, dict.addWord("1", "One"));
 		assertEquals(true, dict.addWord("2", "Two"));
 		assertEquals(true, dict.addWord("3", "Three"));
+	}
+
+	@Test
+	public void testForAddWord2() {
+		assertEquals(false, dict.addWord("", ""));
+	}
+
+	@Test
+	public void testForDeleteWord1() {
+		assertEquals(true, dict.deleteWord("Attitude"));
+		assertEquals(true, dict.deleteWord("A"));
+	}
+
+	@Test
+	public void testForDeleteWord2() {
+		assertEquals(false, dict.deleteWord(""));
+	}
+
+	@Test
+	public void testForGetValue1() {
+		String value = dict.getValue("Attribute");
+		assertEquals("a quality or characteristic that someone or something has", value);
+	}
+
+	@Test
+	public void testForGetValue2() {
+		String value = dict.getValue("");
+		assertEquals(null, value);
+	}
+
+	@Test
+	public void testForSortingOfDictionary() {
+		List<KeyValuePair> list = dict.sortDictionary();
+		assertEquals("1", list.get(0).getkey());
+		assertEquals("Anticipate", list.get(1).getkey());
+		assertEquals("Attitude", list.get(2).getkey());
+		assertEquals("Attribute", list.get(3).getkey());
+		assertEquals("Content", list.get(4).getkey());
+	}
+
+	@Test
+	public void testForSortDictionaryInRange() {
+
+		List<KeyValuePair> list = dict.sortDictionaryInRange("Attitude", "Sophisticated");
+		assertEquals("Attitude", list.get(0).getkey());
+		assertEquals("Attribute", list.get(1).getkey());
+		assertEquals("Content", list.get(2).getkey());
+		assertEquals("Culture", list.get(3).getkey());
+		assertEquals("Sophisticated", list.get(4).getkey());
 	}
 }
