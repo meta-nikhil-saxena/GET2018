f524ac652bd3b51f017516779397fd84056f1423
meta-nikhil-saxena <41181690+meta-nikhil-saxena@users.noreply.github.com>
2018-08-17 19:19:04 +0530

    Changes Made !!

--- DBMS-Assignment-2/Assignment-2.sql
+++ DBMS-Assignment-2/Assignment-2.sql
@@ -1,120 +1,161 @@
-use Storefront;
+USE Storefront;
 /*
 Q1 : 
 Write SQL scripts for the following:
 */
 
 #1 > Insert sample data in StoreFront tables by using SQL files.
-insert into user(User_ID,Email_ID,Name,Password,ContactNum,DOB)
+INSERT INTO user(email_id,name,password,dob)
                  VALUES
-                 ('U1','u1@metacube.com','User1','1234','123345688','1996-08-04'),
-                 ('U2','u2@metacube.com','User2','1234','123345688','1996-08-04'),
-                 ('U3','u3@metacube.com','User3','1234','123345688','1996-08-04'),
-                 ('U4','u4@metacube.com','User4','1234','123345688','1996-08-04'),
-                 ('U5','u5@metacube.com','User5','1234','123345688','1996-08-04'),
-                 ('U6','u6@metacube.com','User6','1234','123345688','1996-08-04'),
-                 ('U7','u7@metacube.com','User7','1234','123345688','1996-08-04');
+                 ('u1@metacube.com','User1','1234','1996-08-04'),
+                 ('u2@metacube.com','User2','1234','1996-08-04'),
+                 ('u3@metacube.com','User3','1234','1996-08-04'),
+                 ('u4@metacube.com','User4','1234','1996-08-04'),
+                 ('u5@metacube.com','User5','1234','1996-08-04'),
+                 ('u6@metacube.com','User6','1234','1996-08-04'),
+                 ('u7@metacube.com','User7','1234','1996-08-04');
                 
 select * from user;
 
-insert into addressbook(User_ID,Address_ID)                           
+INSERT INTO addressbook(user_id)                           
                        VALUES
-                       ('U1','A1'),
-                       ('U2','A2'),
-                       ('U3','A3'),
-                       ('U4','A4'),
-                       ('U5','A5'),
-                       ('U6','A6'),
-                       ('U7','A7');
+                       (1),
+                       (2),
+                       (3),
+                       (4),
+                       (5),
+                       (6),
+                       (7);
                        
 SELECT * FROM addressbook;                       
                        
-insert into addressstore(Address_ID,Address,Pincode,Landmark)
+INSERT INTO addressstore(address_id,address,pincode,landmark)
                           VALUES
-                          ('A1','abcd',123456,''),
-                          ('A2','abcd',123456,'Taj'),
-                          ('A3','abcd',123456,'Taj Hotel'),
-                          ('A4','abcd',123456,'PG'),
-                          ('A5','abcd',123456,'Temple'),
-                          ('A6','abcd',123456,''),
-                          ('A7','abcd',123456,'');
+                          (1,'abcd',123456,''),
+                          (2,'abcd',123456,'Taj'),
+                          (3,'abcd',123456,'Taj Hotel'),
+                          (4,'abcd',123456,'PG'),
+                          (5,'abcd',123456,'Temple'),
+                          (6,'abcd',123456,''),
+                          (7,'abcd',123456,'');
                           
 SELECT * FROM addressstore;
 
-insert into category(Category_ID,Parent_ID,Name,Quantity,Image_logo)
+INSERT INTO category(name,quantity,image_logo)
                      VALUES
-                     ('El','NULL','ELECTRONICS',10,'D:\New folder\GET2018\GET2018\DBMS-Assignment-2'),
-                     ('Mob','El','Mobile',10,'D:\New folder\GET2018\GET2018\DBMS-Assignment-2'),
-                     ('Cl','NULL','Clothes',20,'D:\New folder\GET2018\GET2018\DBMS-Assignment-2'),
-                     ('Et','NULL','Etables',20,'D:\New folder\GET2018\GET2018\DBMS-Assignment-2'),
-                     ('Fd','NULL','Foods',20,'D:\New folder\GET2018\GET2018\DBMS-Assignment-2');
+                     ('ELECTRONICS',10,'D:\New folder\GET2018\GET2018\DBMS-Assignment-2');
+
+INSERT INTO category(parent_id,name,quantity,image_logo)
+                     VALUES                     
+                     (44,'Mobile',10,'D:\New folder\GET2018\GET2018\DBMS-Assignment-2');
+
+INSERT INTO category(name,quantity,image_logo)
+                     VALUES
+                     ('Clothes',20,'D:\New folder\GET2018\GET2018\DBMS-Assignment-2'),
+                     ('Etables',20,'D:\New folder\GET2018\GET2018\DBMS-Assignment-2'),
+                     ('Foods',20,'D:\New folder\GET2018\GET2018\DBMS-Assignment-2'),
+                     ('Snacks',22,'D:\New folder\GET2018\GET2018\DBMS-Assignment-2');
                  
 SELECT * FROM category;
 
-insert into product(Category_ID,Product_ID,Image_ID,Name,Quantity,Cost,Total_images,status)
+INSERT INTO product(name,quantity,cost,status)
                     VALUES
-                    ('El','MB','MBIm','Mobile',5,5000,0,'Available'),
-                    ('El','MC','MCIm','Washing Machine',5,15000,2,'Available'),
-                    ('Cl','SH','SHIm','Shirt',15,500,0,'Available'),
-                    ('Mob','SM','SMIm','Samsung',15,500,0,'Available'),
-                    ('Mob','NO','NOIm','Nokia',15,500,0,'Available'),
-                    ('Fd','FST','FSTIm','Fast Food',15,50,1,'Available');
+                    ('Mobile',5,5000,'Available'),
+                    ('Washing Machine',5,15000,'Available');
                     
-SELECT * FROM product;     
+INSERT INTO product(name,quantity,cost,status)
+                    VALUES         
+                    ('Shirt',15,500,'Available'),
+                    ('Samsung',15,500,'Available');
+                    
+INSERT INTO product(name,quantity,cost,status)
+                    VALUES                    
+                    ('Nokia',15,500,'Available'),
+                    ('Fast Food',15,50,'Available'),
+                    ('Noodles',15,50,'Available');
+                    
+SELECT * FROM category;
+                    
+SELECT * FROM product;  
 
-insert into image(Image_ID,Name,Image)
+INSERT INTO productlist(category_id,product_id)
+                        VALUES
+                        (44,1),
+                        (44,2),
+                        (46,3),
+                        (45,4),
+                        (45,5),
+                        (48,6),
+                        (49,7);
+    
+INSERT INTO productlist(category_id,product_id)
+                        VALUES
+                        (48,7);
+                        
+SELECT * FROM productlist;
+
+
+INSERT INTO image(product_id,name,Image)
                   VALUES
-                  ('MBIm','Mobile','D:\New folder\GET2018\GET2018\DBMS-Assignment-2'),
-                  ('MCIm','Washing Machine','D:\New folder\GET2018\GET2018\DBMS-Assignment-2'),
-                  ('SHIm','T-Shirt','D:\New folder\GET2018\GET2018\DBMS-Assignment-2'),
-                  ('FSTIm','Pasta','D:\New folder\GET2018\GET2018\DBMS-Assignment-2');
+                  (1,'Mobile','D:\New folder\GET2018\GET2018\DBMS-Assignment-2'),
+                  (2,'Washing Machine','D:\New folder\GET2018\GET2018\DBMS-Assignment-2'),
+                  (3,'T-Shirt','D:\New folder\GET2018\GET2018\DBMS-Assignment-2'),
+                  (6,'Pasta','D:\New folder\GET2018\GET2018\DBMS-Assignment-2');
                   
 SELECT * FROM image;      
 
 
 # 2 > Display Id, Title, Category Title, Price of the products which are Active and recently added products should be at top
 
-SELECT P.Product_ID,C.Name,P.Cost
-FROM product P
-INNER JOIN category C
-ON C.Category_ID = P.Category_ID
+SELECT p.id,p.name,c.name AS category_name,p.cost
+FROM product p
+INNER JOIN productlist pl
+ON p.id = pl.product_id
+INNER JOIN category c
+ON c.id = pl.category_id
 WHERE status = 'Available'
-ORDER BY DateAddProduct;
+ORDER BY date DESC;
 
 # 3 > Display the list of products which don't have any images
 
-SELECT Name
+SELECT id,name
 FROM product
-WHERE Total_images = 0;
+WHERE id
+NOT IN(SELECT product_id FROM image);
 
 # 4 > Display all Id, Title and Parent Category Title for all the Categories listed, 
 # sorted by Parent Category Title and then Category Title. 
 # (If Category is top category then Parent Category Title column should display “Top Category” as value.)
 
-SELECT c.Category_ID,c.Name,c.Parent_ID,
-IFNULL( p.Name, 'Parent') AS Parent_Category
+SELECT c.id,c.name,c.parent_id,
+IFNULL(p.name,'Top Category') AS parent_category
 FROM category c
 LEFT JOIN category p
-ON c.Category_ID = p.Parent_ID;
+ON c.id = p.parent_id
+ORDER BY parent_category,c.name;
 
 # 5 > Display Id, Title, Parent Category Title of all the leaf Categories 
 # (categories which are not parent of any other category)
 
-SELECT C.Category_ID, C.Name, 
-IFNULL(P.Name, 'Leaf Categories') as Parent_Category
-FROM Category C                                                                   
-LEFT JOIN Category P
-ON C.Parent_ID = P.Category_ID;
+SELECT c.id, c.name, 
+IFNULL(p.name, 'Leaf Categories') as parent_category
+FROM category c                                                                   
+LEFT JOIN category p
+ON c.parent_id = p.id
+WHERE p.id IS NULL;
+
 
 # 6 > Display Product Title, Price & Description which falls into particular category Title 
 # (i.e. “Mobile”)
 
+SELECT p.id,p.name,p.cost
+FROM product p
+INNER JOIN productlist pl
+ON p.id = pl.product_id
+INNER JOIN category c
+ON c.id = pl.category_id
+WHERE c.name = 'Mobile';
 
-SELECT P.Product_ID,P.Name,P.Cost
-FROM product P
-INNER JOIN category C
-ON P.Category_ID = C.Category_ID
-WHERE C.Name = 'Mobile';
 
 # 7 > Display the list of Products whose Quantity on hand (Inventory) is under 10.
 
